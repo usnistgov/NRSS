a4ebdc8de97ce7bec115359a6009ffcee35418f0
Peter Dudenas <peter.dudenas@nist.gov>
2022-09-07 09:32:10 -0700

    make NRSS pip installable and have PyHyperScattering as a dependency

---
+++ NRSS/__init__.py
@@ -0,0 +1,9 @@
+"""
+NRSS
+
+The NIST RSoXS Simulation Suite: A collection of Python and C++/CUDA tools for simulating and analyzing Resonant Soft X-ray Scattering (RSoXS).
+"""
+
+__version__ = "0.1.0"
+__author__ = "Peter Dudenas"
+__credits__ = "National Institute of Standards and Technology"
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='NRSS',
+    version='0.1.0',
+    description='NIST RSoXS Simulation Suite',
+    url='https://github.com/pdudenas/NRSS',
+    author='Peter Dudenas',
+    author_email='peter.dudenas@nist.gov',
+    license='NIST',
+    install_requires=['PyHyperScattering',
+                        'numpy',
+                        'scipy',
+                        'scikit-image',
+                        'pandas',
+                        'matplotlib',
+                        'h5py'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: NIST',
+        'Operating System :: POSIX :: Linux',
+        'Topic :; Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics'
+        'Programming Language :: Python :: 3'
+    ],
+)
