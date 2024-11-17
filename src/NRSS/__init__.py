f8e1146675837b146b866032a38a8bae064d650e
Dean DeLongchamp <deand@glados>
2024-11-17 21:06:46 +0000

    tweaking __init__.py files to avoid any installation into site-packages

--- src/NRSS/__init__.py
+++ src/NRSS/__init__.py
@@ -1,14 +1,14 @@
-"""
-NRSS
+# """
+# NRSS
 
-The NIST RSoXS Simulation Suite: A collection of Python and C++/CUDA tools for simulating and analyzing Resonant Soft X-ray Scattering (RSoXS).
-"""
+# The NIST RSoXS Simulation Suite: A collection of Python and C++/CUDA tools for simulating and analyzing Resonant Soft X-ray Scattering (RSoXS).
+# """
 
-from NRSS import writer
-from NRSS import checkH5
-from NRSS import morphology
-from NRSS import reader
+# from NRSS import writer
+# from NRSS import checkH5
+# from NRSS import morphology
+# from NRSS import reader
 
-__version__ = "0.1.0"
-__author__ = "Peter Dudenas"
-__credits__ = "National Institute of Standards and Technology"
+# __version__ = "0.1.0"
+# __author__ = "Peter Dudenas"
+# __credits__ = "National Institute of Standards and Technology"
